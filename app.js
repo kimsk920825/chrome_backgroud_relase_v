460b51c4e8b2c22a1395cdd01a72fd721ca0650b
kimseungkyubestiedu <kimseungkyubestiedu@gmail.com>
2022-01-16 20:28:21 +0900

    preventBrowserDefault

--- app.js
+++ app.js
@@ -1,37 +1,9 @@
-const title = document.querySelector("div.hello h1:nth-child(3)");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-console.dir(title);
-//함수 모음
-function handleClickEvent() {
-  title.style.color = "blue";
-}
-
-function handleMouseEnter() {
-  title.innerText = "Mouse has just entered!";
-}
-function handleMouseleave() {
-  title.innerText = "Mouse is gone!";
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
 
-function resizeColorChange() {
-  document.body.style.backgroundColor = "tomato";
-}
-function copyHandler() {
-  alert("just copied!");
-}
-
-function wifiOffLine() {
-  alert("wifi is off!! connect wifi!");
-}
-
-function wifiOnLine() {
-  alert("wifi is on!! good!!");
-}
-//이벤트 리스너 작동 모음
-title.addEventListener("click", handleClickEvent);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseleave);
-window.addEventListener("resize", resizeColorChange);
-window.addEventListener("copy", copyHandler);
-window.addEventListener("offline", wifiOffLine);
-window.addEventListener("online", wifiOnLine);
+loginForm.addEventListener("submit", onLoginSubmit);
