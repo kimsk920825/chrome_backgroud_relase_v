1c46d32628beb8e97d05fc6c6773f4fafc37ce85
kimseungkyubestiedu <kimseungkyubestiedu@gmail.com>
2022-01-16 23:02:48 +0900

    eventhandler advanced

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  console.log(username);
+  greetingHandle(username);
 }
-
-function handleLink(event) {
-  event.preventDefault();
-  console.dir(event);
+function greetingHandle(username) {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerHTML = `Hello ${username}`;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLink);
+
+const usernameValue = localStorage.getItem(USERNAME_KEY);
+
+if (usernameValue === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  greetingHandle(usernameValue);
+}
+
+/*
+form은 수긱고 h1은 표시하라.
+하지만 h1에 표시할 텍스트가 있을때만 표시하라
+브라우저의 기본 동작을 방지하고
+input값을 username에 저장하며
+로그인 폼을 없애주고
+username을 h1태그에 나올 수 있게 세팅하라.
+*/
